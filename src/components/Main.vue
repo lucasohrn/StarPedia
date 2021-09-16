2cfe6334bedd121e80037ebf0f19a0896f31a085
sixtensix <sixtenglarsson@gmail.com>
2021-09-16 09:59:11 +0200

    made searchbar to a component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,46 +1,22 @@
 <template>
   <div class="hello">
     <h1>StarPedia</h1>
-
-    <div class="search">
-      <input type="text" />
-      <button>
-        <img src="../assets/search.png" alt="search" class="searchButton" />
-      </button>
-    </div>
-
-
-    </div>
+    <search-bar></search-bar>
+  </div>
 </template>
 
 <script>
+import SearchBar from './SearchBar.vue';
 export default {
-  name: "Main",
+  components: { 
+    SearchBar 
+    },
+  
   props: {
     msg: String,
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-* {
-  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
-    "Lucida Sans", Arial, sans-serif;
-}
-h1 {
-  color: black;
-  opacity: 0;
-}
-.search {
-  margin-top: 0px;
-}
-.searchButton {
-  max-width: 2em;
-  cursor: pointer;
-}
-.search input {
-  margin-bottom: 20px;
-  height: 2em;
-}
+
 </style>
